445dbfe38b8c1bfed475eaf8024d8c6ad43e2642
earbaj <earbajsaria3@gmail.com>
2023-02-04 20:17:11 -0800

    Add Details Fragment

---
+++ app/src/main/java/com/example/movieapp/details/DetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.movieapp.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+}
